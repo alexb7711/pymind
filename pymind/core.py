c91a2683fb6c36e9979567cb6f85a7c39b99b42d
BROWN <1556048963157002@mil>
2024-09-24 07:23:55 -0600

    Skeleton of the core file, added markdown as a requirement

---
+++ pymind/core.py
@@ -0,0 +1,36 @@
+import logging
+
+logger = logging.getLogger('PYMIND')
+
+class PyMind:
+    """!
+    @brief A tool to convert a directory of markdown files into a structured website!
+
+    The PyMind class is the heart of the application. It contains the front end function calls that the user will
+    interact with. The primary interface will be done via a configuration file so the user simply has to invoke the
+    `pymind` command to generate their personal website.
+    """
+
+    def __init__(self, **kwargs):
+        """!
+        @brief Creates a new PyMind Instance
+
+        @param **kwargs
+        """
+
+        # Read in the configuration if provided
+
+        # Read the arguments provided
+        return
+
+    def pymind(**kwargs: Any):
+        """!
+        @brief Read Markdown files from a directory and write output to `self.out_dir`
+
+        This is a shortcut function which initializes an instace of `PyMind` and calls the `generate_output` function.
+
+        @param kwargs['input'] Path to directory to read from
+        @param kwargs['output'] Path to directory to output to
+        @param kwargs['config'] Configuration file to read from
+        """
+        return
