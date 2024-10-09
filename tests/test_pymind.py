e7cf1ed49eb40d5a953e44a4acc15efd66aa0900
BROWN <1556048963157002@mil>
2024-10-09 08:49:35 -0600

    update count because new file was added to test

--- tests/test_pymind.py
+++ tests/test_pymind.py
@@ -69,7 +69,7 @@ class TestPyMindCore(unittest.TestCase):
         self.assertEqual(pm.input, TestPyMindCore.INPUT)
 
         # Check the number of elements
-        self.assertEqual(len(pm.files_found), 4)
+        self.assertEqual(len(pm.files_found), 5)
 
         return
 
@@ -140,7 +140,7 @@ class TestPyMindCore(unittest.TestCase):
 
         # Count the number of files output
         fc = len(glob.glob(os.path.join(TestPyMindCore.OUTPUT, "*")))
-        self.assertEqual(fc, 4)
+        self.assertEqual(fc, 5)
 
         return
 
