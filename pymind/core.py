6999a80cf86c5a03e2a1dc7206e3e1d1464354d0
Alexander Brown <alex.brown7711@gmail.com>
2024-10-26 13:19:40 -0600

    print var before

--- pymind/core.py
+++ pymind/core.py
@@ -517,6 +517,7 @@ class PyMind:
             "build_files": self.build_files,
             "tags": self.tags,
         }
+        print("=======>", var)
         cache_dir = PyMind.CACHE_PATH / Path("variables")
 
         # Cache the variable
