18846e3b8b32ef24ee284a650f316be825a65be4
BROWN <1556048963157002@mil>
2024-12-10 10:15:19 -0700

    list engine_dir

--- pymind/core.py
+++ pymind/core.py
@@ -408,7 +408,8 @@ class PyMind:
         engine_dir = Path(PyMind.CORE_ENGINE_PATH).absolute()
 
         # List the directories in the engine directory
-        print(os.listdir(os.path.dirname(os.path.abspath(__file__))))
+        # print(os.listdir(os.path.dirname(os.path.abspath(__file__))))
+        print(engine_dir)
         engine_path = [str(x) for x in engine_dir.iterdir() if x.is_dir()]
 
         PRE_PATH = engine_dir / Path("pre")
