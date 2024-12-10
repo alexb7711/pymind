f39a35381c27cb64bb0ad0d1b571c33f50426f23
BROWN <1556048963157002@mil>
2024-12-10 09:28:48 -0700

    import path

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import unittest
+from pathlib import Path
 
 ########################################################################################################################
 
